e7f19f344ddcd4af18b44abadef987fe6f753b49
Bounphone KOSADA <76658219+Bounphone@users.noreply.github.com>
2023-01-30 07:49:38 +0700

    Prop in script

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -5,11 +5,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
 export default defineComponent({
     props : ['title'],
     setup(props) {
         {
+
+            onMounted(() => {
+                console.log(props.title)
+            });
+
             return {}
         }
     },
