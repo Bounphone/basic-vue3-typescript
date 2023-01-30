24fef94fda69db26ee1dcec0ce35d2bb3dbf1c6a
Bounphone KOSADA <76658219+Bounphone@users.noreply.github.com>
2023-01-30 07:57:07 +0700

    Login Form ui

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,22 +1,37 @@
 <template>
-    <div>
-        <h1>{{ title }}</h1>
-    </div>
+  <div>
+    <h1>{{ title }}</h1>
+    <form action="">
+      <!-- username -->
+      <label for="">User name</label>
+      <input type="text" name="username" id="password" />
+      <br />
+
+      <!-- password -->
+      <label for="">Password</label>
+      <input type="text" name="password" id="password" />
+      <br />
+      <!-- Debug -->
+      <span>Debug: {{ state.account }}</span>
+      <!-- button -->
+      <button>Submit</button>
+      <button>Clear</button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, onMounted, reactive } from "vue";
 export default defineComponent({
-    props : ['title'],
-    setup(props) {
-        {
+  props: ["title"],
+  setup(props) {
+    {
+      const state = reactive({ account: { username: "", password: "" } });
 
-            onMounted(() => {
-                console.log(props.title)
-            });
-
-            return {}
-        }
-    },
-})
+      return {
+        state,
+      };
+    }
+  },
+});
 </script>
